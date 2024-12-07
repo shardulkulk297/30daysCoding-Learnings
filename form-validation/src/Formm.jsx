1033424196646c0e38ca94a95fc565609d779aba
sahilmendgule03@gmail.com <shardulkulk297@gmail.com>
2024-12-08 00:32:02 +0530

    Validating form

--- form-validation/src/Formm.jsx
+++ form-validation/src/Formm.jsx
@@ -1,37 +1,67 @@
 import React from 'react'
-
+import { useState } from 'react'
 const Formm = () => {
-  return (
+  const [name, setname] = useState('')
+  const [email, setemail] = useState('')
+  const [password, setpassword] = useState('')
+  const [gender, setgender] = useState('')
+  
+
+
+
+    const handleSubmit = (e)=>{
+
+        e.preventDefault();
+        console.log("Form Submitted Successfully");
+    }
+  
+    return (
     <div className='container'>
 
         
         <h1>Form Validation</h1>
 
-        <form action="">
+        <form onSubmit={handleSubmit}>
 
             <div>
-                <label htmlFor="">Name</label>
-                <input type="text" />
+                <label htmlFor="" >Name</label>
+                <input onChange={(e)=>{
+                    setname(e.target.value);
+                }} type="text" />
             </div>
 
             <div>
-                <label htmlFor="">Email</label>
+                <label onChange={(e)=>{
+                    setemail(e.target.value)
+                }} htmlFor="">Email</label>
                 <input type="email" />
             </div>
 
             <div>
-                <label htmlFor="">Password</label>
-                <input type="password" />
+                <label htmlFor="" onChange={(e)=>{
+                    setpassword(e.target.value)
+                }}>Password</label>
+                <input  type="password" />
             </div>
             
             <div>
                 <label htmlFor="">Confirm Password</label>
-                <input type="email" />
+                <input  type="password" />
             </div>
             
             <div>
-                <label htmlFor="">Email</label>
-                <input type="email" />
+                <label htmlFor="" >Gender</label>
+                <select onChange={()=>{
+                    setgender(e.target.value)
+                }} name="" id="">
+                    <option value="">Male</option>
+                    <option value="">Female</option>
+                    
+                </select>
+            </div>
+
+            <div>
+                <button type='submit'>Submit</button>
             </div>
         </form>
 
