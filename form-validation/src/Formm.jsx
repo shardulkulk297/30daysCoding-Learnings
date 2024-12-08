14b7046e4955641f09e6c63b5269f9e99cff6bf1
sahilmendgule03@gmail.com <shardulkulk297@gmail.com>
2024-12-08 21:44:13 +0530

    Completed form validation and we can do it using the useform hok

--- form-validation/src/Formm.jsx
+++ form-validation/src/Formm.jsx
@@ -5,14 +5,54 @@ const Formm = () => {
   const [email, setemail] = useState('')
   const [password, setpassword] = useState('')
   const [gender, setgender] = useState('')
-  
+  const [error, seterror] = useState('')
 
+    const validateForm = ()=>{
+        const errors = {};
+
+        if(!name)
+        {
+            errors.name = "Name is required";
+        }
+
+        if(!email)
+        {
+            errors.email = "email is required";
+        }
+        else if(!/\S+@\S+\.\S+/.test(email))
+        {
+            errors.email = "Email is Invalid";
+        }
+
+        if(!password)
+        {
+            errors.password = "Password is required"
+        }
+        else if(password.length <= 6)
+        {
+            errors.password = "Password length must be greater than 6";
+        }
+
+        console.log(errors);
+        return errors;
+
+    }
 
 
     const handleSubmit = (e)=>{
 
         e.preventDefault();
+        const validationErrors = validateForm();
+
+        if(Object.keys(validationErrors).length > 0)
+        {
+            seterror(validationErrors)
+
+        }
+        console.log(error);
         console.log("Form Submitted Successfully");
+
+        
     }
   
     return (
@@ -28,25 +68,28 @@ const Formm = () => {
                 <input onChange={(e)=>{
                     setname(e.target.value);
                 }} type="text" />
+                {error.name}
             </div>
 
             <div>
-                <label onChange={(e)=>{
+                <label  htmlFor="">Email</label>
+                <input onChange={(e)=>{
                     setemail(e.target.value)
-                }} htmlFor="">Email</label>
-                <input type="email" />
+                }} type="email" />
+                 {error.email}
             </div>
 
             <div>
-                <label htmlFor="" onChange={(e)=>{
+                <label htmlFor="" >Password</label>
+                <input onChange={(e)=>{
                     setpassword(e.target.value)
-                }}>Password</label>
-                <input  type="password" />
+                }}  type="password" />
+                 {error.password}
             </div>
             
             <div>
                 <label htmlFor="">Confirm Password</label>
-                <input  type="password" />
+                <input   type="password" />
             </div>
             
             <div>
